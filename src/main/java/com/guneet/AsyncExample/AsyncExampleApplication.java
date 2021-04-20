4983af2b074c0a27eefe92331ba76401ddd27f37
Guneet Singh <guneetnagia24@gmail.com>
2021-04-20 21:22:06 +0530

    adding executor service

--- src/main/java/com/guneet/AsyncExample/AsyncExampleApplication.java
+++ src/main/java/com/guneet/AsyncExample/AsyncExampleApplication.java
@@ -1,14 +1,30 @@
 package com.guneet.AsyncExample;
 
+import java.util.concurrent.Executor;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @SpringBootApplication
+@EnableAsync
 public class AsyncExampleApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(AsyncExampleApplication.class, args);
 	}
 
+	@Bean
+	public Executor asyncExecutor(){
+		ThreadPoolTaskExecutor executor  =new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(2);
+		executor.setMaxPoolSize(2);
+		executor.setQueueCapacity(500);
+		executor.setThreadNamePrefix("Guneet-");
+		executor.initialize();
+		return executor;
+	}
 
 }
